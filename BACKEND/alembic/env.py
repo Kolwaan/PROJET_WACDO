de4975ab20b59c60e2f66ae126348bb3c82c7e27
Kolwaan <nicolas-colin@outlook.fr>
2026-04-10 14:18:25 +0200

    maj env.py

--- BACKEND/alembic/env.py
+++ BACKEND/alembic/env.py
@@ -19,6 +19,11 @@ from app.models.order_menu import order_menus
 # access to the values within the .ini file in use.
 config = context.config
 
+import os
+db_url = os.environ.get("DATABASE_URL")
+if db_url:
+    config.set_main_option("sqlalchemy.url", db_url)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
